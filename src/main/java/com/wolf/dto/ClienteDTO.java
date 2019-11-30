333a8f93c82d2e408219724b7defb740ad1e5b27
mlobo-dev <mlobo.dev@gmail.com>
2019-11-30 18:52:08 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/wolf/dto/ClienteDTO.java
@@ -0,0 +1,59 @@
+package com.wolf.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.wolf.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer Id;
+	@NotEmpty(message ="Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	@NotEmpty(message ="Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		Id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
