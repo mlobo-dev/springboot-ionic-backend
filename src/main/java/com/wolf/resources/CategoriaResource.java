63d14cb5a9a102501c87866ba5605806a9b097c0
mlobo-dev <mlobo.dev@gmail.com>
2019-11-29 22:18:20 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/wolf/resources/CategoriaResource.java
+++ src/main/java/com/wolf/resources/CategoriaResource.java
@@ -1,10 +1,13 @@
 package com.wolf.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wolf.domain.Categoria;
+import com.wolf.dto.CategoriaDTO;
 import com.wolf.service.CategoriaService;
 
 @RestController
@@ -49,5 +53,12 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
