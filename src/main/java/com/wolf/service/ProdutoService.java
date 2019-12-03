bb75e5a3c912d4088a26dabc8e3ad044646a84eb
mlobo-dev <mlobo.dev@gmail.com>
2019-12-02 21:34:35 -0300

    Busca de produtos por um trcho do nome e pela categoria

--- src/main/java/com/wolf/service/ProdutoService.java
+++ src/main/java/com/wolf/service/ProdutoService.java
@@ -1,11 +1,17 @@
 package com.wolf.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.wolf.domain.Categoria;
 import com.wolf.domain.Produto;
+import com.wolf.repositories.CategoriaRepository;
 import com.wolf.repositories.ProdutoRepository;
 import com.wolf.service.exception.ObjectNotFoundException;
 
@@ -15,13 +21,31 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repository;
 
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
 	public Produto buscar(Integer id) {
 		Optional<Produto> obj = repository.findById(id);
-		if(obj.isPresent()) {
+		if (obj.isPresent()) {
 			return obj.get();
-		}else {
+		} else {
 			throw new ObjectNotFoundException("Id não encontrado");
 		}
-			
+	}
+
+	/*
+	 * public Page<Produto> search(String nome, List<Integer> ids, Integer page,
+	 * Integer linesPerPage, String orderBy, String direction) { PageRequest
+	 * pageRequest = PageRequest.of(page, linesPerPage,
+	 * Direction.valueOf(direction), orderBy); List<Categoria> categorias =
+	 * categoriaRepository.findAllById(ids); return repository.search(nome,
+	 * categorias, pageRequest); }
+	 */
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.findDisctinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
 	}
 }
