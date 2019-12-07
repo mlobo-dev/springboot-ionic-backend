a3637ce1b173e60c46a0e9a9233b7d71224964c6
mlobo-dev <mlobo.dev@gmail.com>
2019-12-07 18:46:09 -0300

    Implementando SmtpEmailService com servidor do google

--- src/main/java/com/wolf/config/DevConfig.java
+++ src/main/java/com/wolf/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.wolf.service.DbService;
+import com.wolf.service.EmailService;
+import com.wolf.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
